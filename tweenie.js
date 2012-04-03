e06d8ffd5ea65d40a4219e3225118ce260005273
Jacob Buck <buck.jacob@gmail.com>
2012-04-03 12:00:26 +1200

    Renamed some variables (for no particular reason).

--- tweenie.js
+++ tweenie.js
@@ -8,32 +8,32 @@
 	
 	var queue = [];
 	
-	function render (t) {
+	function render (time) {
 		for (var i = 0; i < queue.length; i++)
-			queue[i](t);
+			queue[i](time);
 		queue.length && requestAnimationFrame(render);
 	}
 	
-	function add_queue (f) {
-		queue.push(f) === 1 && requestAnimationFrame(render);
+	function add_queue (fn) {
+		queue.push(fn) === 1 && requestAnimationFrame(render);
 	}
 	
-	function rem_queue (f) {
+	function rem_queue (fn) {
 		for (var i = 0; i < queue.length; i++)
-			f === queue[i] && queue.splice(i, 1);
+			fn === queue[i] && queue.splice(i, 1);
 	}
 	
 	window.tweenie = function (duration, fn, from, to, complete, easing) {
 		var me = this,
 			start = +new Date(),
-			finish = start + duration,
+			end = start + duration,
 			stop = 0,
 			from = from || 0,
 			to = to || 1,
 			easing = easing || function (t) { return Math.sin(t * Math.PI / 2); },
 			run = function (time) {
-				fn((easing((stop || time > finish ? 1 : (time - start) / duration)) * (to - from)) + from);
-				if (stop || time > finish) {
+				fn((easing((stop || time > end ? 1 : (time - start) / duration)) * (to - from)) + from);
+				if (stop || time > end) {
 					rem_queue(run);
 					complete && complete();
 				}
