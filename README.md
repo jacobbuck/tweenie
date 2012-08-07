00125f860cc28b14386a3eb121d6994f8bbd136f
Jacob Buck <buck.jacob@gmail.com>
2012-08-08 10:49:49 +1200

    Removed tweenie-mini.js. Updates to stop/kill functions (see readme). Cleaned up code structure.

--- README.md
+++ README.md
@@ -5,22 +5,25 @@ A small (0.4 KB gzipped) tweening libary using the [requestAnimationFrame](http:
 ### Usage
 
 ``` js
-var tween = tweenie(
+var mytween = tweenie(
 	1000, // duration
-	function (position) { // do something at position
+	function ( position ) { // do something at position
 		thing.style.top = position + "px";
 	},
 	100, // start
 	300, // end
 	function () { // do something on completion (optional)
-		console.log("done");
+		console.log('done');
 	},
-	function (t,b,c,d) { return fancy math equation; } // easing function (optional)
+	function (t, b, c, d) { return fancy math equation; } // easing equation (optional)
 );
 // stop and end a tween
-tween.stop();
+mytween.end();
 
-// kill all existing tweens
+// stop and end all existing tweens
+tweenie.end();
+
+// kill (stop but not end) all existing tweens
 tweenie.kill();
 ```
 
@@ -28,10 +31,6 @@ tweenie.kill();
 
 Will run on all [Grade A browsers](http://yuilibrary.com/yui/docs/tutorials/gbs/).
 
-### tweenie-mini.js
-
-A slighty smaller version for when you're already using a [requestAnimationFrame polyfill](https://gist.github.com/1579671) in your project.
-
 ### Inspired by
 
-[Morpheus](https://github.com/ded/morpheus/) by Dustin Diaz, [Émile](https://github.com/madrobby/emile/) by Thomas Fuchs, and [this blog post](http://paulirish.com/2011/requestanimationframe-for-smart-animating/) by Paul Irish.
+[Émile](https://github.com/madrobby/emile/) by Thomas Fuchs, [Morpheus](https://github.com/ded/morpheus/) by Dustin Diaz, and [this blog post](http://paulirish.com/2011/requestanimationframe-for-smart-animating/) by Paul Irish.
--- tweenie.js
+++ tweenie.js
@@ -3,58 +3,68 @@
  * https://github.com/jacobbuck/tweenie.js
  * Licensed under the terms of the MIT license.
  */
-window.tweenie = function (win) {
-	
-	var request_frame = function () { // requestAnimationFrame polyfill - adapted from https://gist.github.com/1579671
+window.tweenie = function ( window ) {
+		
+	var endall,
+		queue = [],
+		request_frame = function () { // requestAnimationFrame polyfill - adapted from https://gist.github.com/1579671
 			var lastTime = 0,
-				vendors = ["r", "msR", "mozR", "webkitR", "oR"],
-				i,
+				vendors = ['r', 'msR', 'mozR', 'webkitR', 'oR'],
+				i = 0,
 				val;
-			for (; val = vendors[i]+"equestAnimationFrame", i < vendors.length; i++)
-				if (val in win) return win[val];
-			return function (callback, element) {
+			for ( ; val = vendors[i] + 'equestAnimationFrame', i < vendors.length; i++ )
+				if ( val in window ) return window[ val ];
+			return function ( callback, element ) {
 				var currTime = +new Date(),
-					timeToCall = Math.max(0, 16 - (currTime - lastTime)),
-					id = win.setTimeout(function() { callback(currTime + timeToCall); }, 
-				  timeToCall);
+					timeToCall = Math.max( 0, 16 - ( currTime - lastTime ) ),
+					id = window.setTimeout( function() { callback( currTime + timeToCall ); }, timeToCall );
 				lastTime = currTime + timeToCall;
 				return id;
 			};
-		} (),
-		queue = [],
-		render_queue = function (time) {
-			for (var i = 0; i < queue.length; i++)
-				queue[i](time);
-			queue.length && request_frame(render_queue);
-		},
-		add_queue = function (fn) {
-			queue.push(fn) === 1 && request_frame(render_queue);
-		},
-		rem_queue = function (fn) {
-			for (var i = 0; i < queue.length; i++)
-				fn === queue[i] && queue.splice(i, 1);
-		},
-		empty_queue = function () {
-			queue = [];
-		},
-		tweenie = function (duration, fn, from, to, complete, easing) {
-			var start,
-				stop,
-				easing = easing || function (t,b,c,d) { return c * Math.sin(t/d * (Math.PI/2)) + b; }, // sine ease out
-				run = function (time) {
-					start = start || time;
-					fn(stop || time > start + duration ? to : easing(time - start, 0, 1, duration) * (to - from) + from);
-					if (stop || time > start + duration)
-						rem_queue(run) || complete && complete();
-				};
-			add_queue(run);
-			return {
-				stop : function () { stop = 1; }
-			};
-		};
+		} ();
 	
-	tweenie.kill = empty_queue;
+	function render ( time ) {
+		for ( var i = 0; i < queue.length; i++ )
+			queue[ i ]( time );
+		queue.length && request_frame( render );
+	}
+	
+	function add ( fn ) {
+		1 === queue.push( fn ) && request_frame( render );
+	}
+	
+	function remove ( fn ) {
+		for ( var i = 0; i < queue.length; i++ )
+			fn === queue[ i ] && queue.splice( i, 1 );
+		if ( ! queue.length )
+			endall = 0;
+	}
+	
+	function tweenie ( duration, step, from, to, callback, easing ) {
+		var start,
+			end,
+			easing = easing || function (t,b,c,d) { return c * Math.sin(t/d * (Math.PI/2)) + b; }, // sine ease out
+			run = function ( time ) {
+				end = end || endall;
+				start = start || time;
+				step( end || time > start + duration ? to : easing( time - start, 0, 1, duration ) * ( to - from ) + from );
+				if ( end || time > start + duration )
+					remove( run ) || callback && callback();
+			};
+		add( run );
+		return {
+			end : function () { end = 1; }
+		};
+	}
+	
+	tweenie.end = function () {
+		endall = 1;	
+	}
+	
+	tweenie.kill = function () {
+		queue = [];
+	}
 	
 	return tweenie;
 	
-} (this);
+} ( this );
